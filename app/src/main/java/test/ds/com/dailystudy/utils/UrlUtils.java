e1b86e9e98593fc853fe1dcb0e93dbc44dd3f055
lvzhuozhao <785709170@qq.com>
2017-01-16 17:04:54 +0800

    修改后  lv

--- app/src/main/java/test/ds/com/dailystudy/utils/UrlUtils.java
+++ app/src/main/java/test/ds/com/dailystudy/utils/UrlUtils.java
@@ -1,13 +1,17 @@
 package test.ds.com.dailystudy.utils;
 
 /**
- * Created by 吕卓钊
- * on 2017/1/14 11:19.
+ * Created by 张天成
+ * on 2017/1/11 18:45.
  */
-
 public class URLUtils {
+    public static String sort_url = "http://www.meirixue.com/";
+    public static String sort_canshu = "api.php?c=category&a=getall";
+    public static String sort_list_url = "http://www.meirixue.com/";
+    public static String sort_list_canshu = "api.php?c=list&a=index&order=&p=1&isfree=&by=desc&cid=";
+
     //    主页面Url
     public static final String homeUrl = "http://www.meirixue.com/";
     public static final String homeArgs = "http://www.meirixue.com/api.php?a=indexv9&c=index";
-   // public static final String home = "http://www.meirixue.com/api.php?a=indexv9&c=index";
+    // public static final String home = "http://www.meirixue.com/api.php?a=indexv9&c=index";
 }
